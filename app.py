f0b2249ba6d384beab35dea57f18dea35f8c58ee
toshan kanwar <162002505+toshankanwar@users.noreply.github.com>
2025-05-19 12:15:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/add', methods=['POST'])
+def add_numbers():
+    data = request.get_json()
+    num1 = float(data['num1'])
+    num2 = float(data['num2'])
+    result = num1 + num2
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
